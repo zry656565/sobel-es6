9b141d7c290615376276f71c98ebd159a4388a47
Jerry Zou <zry656565@gmail.com>
2015-10-13 01:14:25 +0800

    sobel-es6 0.0.1 completed

--- build/sobel.js
+++ build/sobel.js
@@ -3,82 +3,91 @@
  * @email: jerry.zry@outlook.com
  */
 
-'use strict';
+"use strict";
 
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 (function () {
-  var Image = (function () {
-    function Image(imageData) {
-      _classCallCheck(this, Image);
+  var MyImage = (function () {
+    function MyImage(imageData) {
+      _classCallCheck(this, MyImage);
 
-      this.data = imageData.data.slice(0);
+      this.data = imageData.data;
+      if (!imageData.fake) this.data = this.data.slice(0);
       this.w = imageData.width;
       this.h = imageData.height;
     }
 
-    _createClass(Image, [{
-      key: 'get',
-      value: function get(x, y) {
-        var base = this.w * y + x;
-        return [this.data[base], this.data[base + 1], this.data[base + 2], this.data[base + 3]];
-      }
-    }, {
-      key: 'toGrayscale',
-      value: function toGrayscale() {
-        for (var i = 0; i < this.data.length; i += 4) {
-          var avg = (this.data[i] + this.data[i + 1] + this.data[i + 2]) / 3;
-          this.data[i] = this.data[i + 1] = this.data[i + 2] = avg;
+    _createClass(MyImage, [{
+      key: "pixel",
+      value: function pixel(x, y, value) {
+        var base = (this.w * y + x) * 4;
+        if (!value) {
+          return [this.data[base], this.data[base + 1], this.data[base + 2], this.data[base + 3]];
+        } else {
+          for (var i = 0; i < 4; i++) {
+            this.data[base + i] = value[i];
+          }
         }
       }
     }, {
-      key: 'toImageData',
-      value: function toImageData() {
+      key: "grayscale",
+      value: function grayscale() {
+        var grayscaleData = [];
+        for (var i = 0; i < this.data.length; i += 4) {
+          var avg = (this.data[i] + this.data[i + 1] + this.data[i + 2]) / 3;
+          grayscaleData[i] = grayscaleData[i + 1] = grayscaleData[i + 2] = avg;
+          grayscaleData[i + 3] = 255;
+        }
+        return new MyImage({
+          fake: true,
+          data: grayscaleData,
+          width: this.w,
+          height: this.h
+        });
+      }
+    }, {
+      key: "imageData",
+      value: function imageData() {
         return new ImageData(new Uint8ClampedArray(this.data), this.w, this.h);
       }
     }]);
 
-    return Image;
+    return MyImage;
   })();
 
-  var Sobel = (function () {
-    function Sobel(imageData) {
-      _classCallCheck(this, Sobel);
+  function sobel(imageData) {
+    var begin = Date.now();
+    var image = new MyImage(imageData).grayscale();
+    var edgeData = new MyImage({
+      fake: true,
+      data: [],
+      width: image.w,
+      height: image.h
+    });
 
-      var image = this.image = new Image(imageData);
-      image.toGrayscale();
+    var operatorX = [[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]];
+    var operatorY = [[-1, -2, -1], [0, 0, 0], [1, 2, 1]];
 
-      var operatorX = [[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]];
-      var operatorY = [[-1, -2, -1], [0, 0, 0], [1, 2, 1]];
-
-      //function mult(operator, x, y) {
-      //  return (operator[0][0] * image.get(x-1, y-1)
-      //    + operator[0][1] * image.get(x-1, y)
-      //    + operator[0][2] * image.get(x-1, y+1)
-      //    + operator[1][0] * image.get(x-1, y)
-      //    + operator[1][1] * image.get(x-1, y)
-      //    + operator[1][2] * image.get(x-1, y)
-      //    + operator[2][0] * image.get(x-1, y+1)
-      //    + operator[2][1] * image.get(x-1, y+1)
-      //    + operator[2][2] * image.get(x-1, y+1))
-      //}
+    function multiple(operator, x, y) {
+      return operator[0][0] * image.pixel(x - 1, y - 1)[0] + operator[0][1] * image.pixel(x, y - 1)[0] + operator[0][2] * image.pixel(x + 1, y - 1)[0] + operator[1][0] * image.pixel(x - 1, y)[0] + operator[1][1] * image.pixel(x, y)[0] + operator[1][2] * image.pixel(x + 1, y)[0] + operator[2][0] * image.pixel(x - 1, y + 1)[0] + operator[2][1] * image.pixel(x, y + 1)[0] + operator[2][2] * image.pixel(x + 1, y + 1)[0];
     }
 
-    _createClass(Sobel, [{
-      key: 'getResultArray',
-      value: function getResultArray() {}
-    }, {
-      key: 'renderResult',
-      value: function renderResult(canvas) {
-        var ctx = canvas.getContext('2d');
-        ctx.putImageData(this.image.toImageData(), 0, 0);
+    for (var y = 0; y < image.h; y++) {
+      for (var x = 0; x < image.w; x++) {
+        var Gx = multiple(operatorX, x, y);
+        var Gy = multiple(operatorY, x, y);
+        var result = Math.sqrt(Gx * Gx + Gy * Gy);
+        edgeData.pixel(x, y, [result, result, result, 255]);
       }
-    }]);
+    }
 
-    return Sobel;
-  })();
+    console.log("Sobel takes: " + (Date.now() - begin) + " ms");
 
-  window.Sobel = Sobel;
+    return edgeData;
+  }
+
+  window.sobel = sobel;
 })();
--- sobel.js
+++ sobel.js
@@ -4,74 +4,98 @@
  */
 
 (function(){
-  class Image {
+  class MyImage {
     constructor(imageData) {
-      this.data = imageData.data.slice(0)
+      this.data = imageData.data
+      if (!imageData.fake) this.data = this.data.slice(0)
       this.w = imageData.width
       this.h = imageData.height
     }
 
-    get(x, y) {
-      let base = this.w * y + x
-      return [
-        this.data[base],
-        this.data[base + 1],
-        this.data[base + 2],
-        this.data[base + 3]
-      ]
-    }
-
-    toGrayscale() {
-      for (let i = 0; i < this.data.length; i+= 4) {
-        let avg = (this.data[i] + this.data[i+1] + this.data[i+2]) / 3
-        this.data[i] = this.data[i+1] = this.data[i+2] = avg
+    pixel(x, y, value) {
+      let base = (this.w * y + x) * 4
+      if (!value) {
+        return [
+          this.data[base],
+          this.data[base + 1],
+          this.data[base + 2],
+          this.data[base + 3]
+        ]
+      } else {
+        for (let i = 0; i < 4; i++) {
+          this.data[base + i] = value[i]
+        }
       }
     }
 
-    toImageData() {
+    grayscale() {
+      let grayscaleData = []
+      for (let i = 0; i < this.data.length; i+= 4) {
+        let avg = (this.data[i] + this.data[i+1] + this.data[i+2]) / 3
+        grayscaleData[i] = grayscaleData[i+1] = grayscaleData[i+2] = avg
+        grayscaleData[i+3] = 255
+      }
+      return new MyImage({
+        fake: true,
+        data: grayscaleData,
+        width: this.w,
+        height: this.h
+      })
+    }
+
+    imageData() {
       return new ImageData(new Uint8ClampedArray(this.data), this.w, this.h)
     }
   }
 
-  class Sobel {
-    constructor(imageData) {
-      let image = this.image = new Image(imageData)
-      image.toGrayscale()
+  function sobel(imageData) {
+    const begin = Date.now()
+    let image = (new MyImage(imageData)).grayscale()
+    let edgeData = new MyImage({
+      fake: true,
+      data: [],
+      width: image.w,
+      height: image.h
+    })
 
-      const operatorX = [
-        [-1,0,1],
-        [-2,0,2],
-        [-1,0,1]
-      ]
-      const operatorY = [
-        [-1,-2,-1],
-        [0,0,0],
-        [1,2,1]
-      ]
+    const operatorX = [
+      [-1,0,1],
+      [-2,0,2],
+      [-1,0,1]
+    ]
+    const operatorY = [
+      [-1,-2,-1],
+      [0,0,0],
+      [1,2,1]
+    ]
 
-      //function mult(operator, x, y) {
-      //  return (operator[0][0] * image.get(x-1, y-1)
-      //    + operator[0][1] * image.get(x-1, y)
-      //    + operator[0][2] * image.get(x-1, y+1)
-      //    + operator[1][0] * image.get(x-1, y)
-      //    + operator[1][1] * image.get(x-1, y)
-      //    + operator[1][2] * image.get(x-1, y)
-      //    + operator[2][0] * image.get(x-1, y+1)
-      //    + operator[2][1] * image.get(x-1, y+1)
-      //    + operator[2][2] * image.get(x-1, y+1))
-      //}
-
+    function multiple(operator, x, y) {
+      return (
+        operator[0][0] * image.pixel(x-1, y-1)[0] +
+        operator[0][1] * image.pixel(x,   y-1)[0] +
+        operator[0][2] * image.pixel(x+1, y-1)[0] +
+        operator[1][0] * image.pixel(x-1, y)[0]   +
+        operator[1][1] * image.pixel(x,   y)[0]   +
+        operator[1][2] * image.pixel(x+1, y)[0]   +
+        operator[2][0] * image.pixel(x-1, y+1)[0] +
+        operator[2][1] * image.pixel(x,   y+1)[0] +
+        operator[2][2] * image.pixel(x+1, y+1)[0]
+      )
     }
 
-    getResultArray() {
-
+    for (let y = 0; y < image.h; y++) {
+      for (let x = 0; x < image.w; x++) {
+        let Gx = multiple(operatorX, x, y)
+        let Gy = multiple(operatorY, x, y)
+        let result = Math.sqrt(Gx*Gx + Gy*Gy)
+        edgeData.pixel(x, y, [result, result, result, 255])
+      }
     }
 
-    renderResult(canvas) {
-      let ctx = canvas.getContext('2d')
-      ctx.putImageData(this.image.toImageData(), 0, 0)
-    }
+    console.log(`Sobel takes: ${Date.now() - begin} ms`)
+
+    return edgeData
   }
 
-  window.Sobel = Sobel
+  window.sobel = sobel
 }())
